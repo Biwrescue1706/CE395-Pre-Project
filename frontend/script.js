1911d598103255badd21f56851c346a9bc3cb875
Wuttichai <tondevjourney.wp@gmail.com>
2025-04-02 04:23:38 +0700

    Create script.js
    frontend ดึงข้อมูลทุก 5 วิ

---
+++ frontend/script.js
@@ -0,0 +1,22 @@
+const API_URL = "http://localhost:3000/latest";
+
+async function fetchSensorData() {
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    document.getElementById("light").textContent = data.light;
+    document.getElementById("temp").textContent = data.temp;
+    document.getElementById("humidity").textContent = data.humidity;
+
+    const now = new Date().toLocaleString();
+    document.getElementById("timestamp").textContent = now;
+  } catch (error) {
+    console.error("เกิดข้อผิดพลาดในการดึงข้อมูล:", error);
+  }
+}
+
+// โหลดครั้งแรก
+fetchSensorData();
+// Frontend โหลดข้อมูลทุก 5 วิ
+setInterval(fetchSensorData, 5000);
